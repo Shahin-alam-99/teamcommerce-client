2eb3a94cc3a7363279aac843a3a3ec67fd8e6325
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-03 18:15:31 +0600

    error page added and react router added

---
+++ src/Components/Home/HomeContainer/HomeContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderContainer from "../../Common/Shared/Header/HeaderContainer/HeaderContainer";
+import Carousel from "../Carousel/Carousel";
+
+const HomeContainer = () => {
+  return (
+    <>
+      <HeaderContainer />
+      <Carousel />
+    </>
+  );
+};
+
+export default HomeContainer;
