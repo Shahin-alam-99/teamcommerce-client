bcc1d0e8d4296a7003230494dd12cf2bfc3fb2d9
arifulislam10111999 <arifulislam10111999@gmail.com>
2022-01-03 20:56:43 +0600

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
+
+import Carousel from './Components/Home/Carousel/Carousel';
 import "./App.css";
 import HeaderContainer from "./Components/Common/Shared/Header/HeaderContainer/HeaderContainer";
 function App() {
   return (
     <div className="App">
       <HeaderContainer />
-=======
 
-import './App.css';
-import Carousel from './Components/Home/Carousel/Carousel';
-
-
-
-function App() {
-  return (
-    <div className="App">
       <Carousel></Carousel>
-     
->>>>>>> ee3f09b12d934b0e97f3b5d985832005c30ce800
-    </div>
-  );
-}
+
+     </div>
+
+  )}
 
 export default App;
