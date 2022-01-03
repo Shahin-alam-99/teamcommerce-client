8e0f4f6d33386caf870869253804f90d78396f2e
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-03 20:21:42 +0600

    main catagory card updated

--- src/Components/Home/TopCatagories/TopCatagories.js
+++ src/Components/Home/TopCatagories/TopCatagories.js
@@ -1,4 +1,4 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 import Slider from "react-slick";
 import image from "../../../images/shoe.jpg";
@@ -12,19 +12,32 @@ const TopCatagories = () => {
     slidesToScroll: 1,
   };
   return (
-    <Container maxWidth="xl" sx={{ my: 5 }}>
-      <Typography variant="h4" sx={{ mb: 1 }}>
-        Top Catagories
-      </Typography>
-      <Slider {...settings}>
-        {[...Array(20)].map((p) => (
-          <div key={p}>
-            <img className="secondary-hover-effect" src={image} alt="" />
-            <h2>Catagory{p}</h2>
-          </div>
-        ))}
-      </Slider>
-    </Container>
+    <Box sx={{ background: "#F1F1F1", py: 5, my: 15 }}>
+      <Container maxWidth="xl">
+        <Typography variant="h4" sx={{ mb: 4 }}>
+          Top Catagories
+        </Typography>
+        <Slider {...settings}>
+          {[...Array(20)].map((p) => (
+            <Box
+              className="secondary-hover-effect"
+              key={p}
+              sx={{
+                background: "white",
+                width: "200px !important",
+                p: 3,
+                borderRadius: "5px",
+              }}
+            >
+              <Box sx={{ overflow: "hidden" }}>
+                <img style={{ width: "100%" }} src={image} alt="" />
+              </Box>
+              <h2>Catagory{p}</h2>
+            </Box>
+          ))}
+        </Slider>
+      </Container>
+    </Box>
   );
 };
 
