4112bf19b664c38e7ef566b87370211459e2d337
arifulislam10111999 <arifulislam10111999@gmail.com>
2022-01-03 15:31:16 +0600

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Carousel from './Components/Home/Carousel/Carousel';
+
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Carousel></Carousel>
+     
     </div>
   );
 }
