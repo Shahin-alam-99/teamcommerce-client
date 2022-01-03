b5724e521efe5aa2fa30b8e69ce684c44fc4bf67
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-03 22:39:26 +0600

    top catagory and home catagory section added

---
+++ src/Components/Home/HomeCatagorySection/HomeCatagorySection.js
@@ -0,0 +1,73 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Rating,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import banner from "../../../images/product-banner.png";
+import product from "../../../images/shoe.jpg";
+
+const HomeCatagorySection = () => {
+  return (
+    <Container sx={{ mb: 5 }} maxWidth="xl">
+      {/*================
+       category names
+       ============== */}
+      <Box sx={{ display: "flex" }}>
+        <Typography sx={{ fontWeight: 600, mb: 1 }} variant="h2">
+          Shoes
+        </Typography>
+      </Box>
+
+      <Grid container spacing={{ xs: 1, md: 5 }}>
+        <Grid item sm={12} md={4} lg={4}>
+          <img style={{ height: "100%" }} src={banner} alt="" />
+        </Grid>
+        <Grid item sm={12} md={8} lg={8}>
+          <Grid container spacing={{ md: 2, xs: 1 }}>
+            {[...Array(8)].map((p) => (
+              <Grid item md={3} lg={3} sm={4} xs={12}>
+                <Card>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={product}
+                    alt="green iguana"
+                  />
+                  <CardContent sx={{ textAlign: "center", pb: "0 !important" }}>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Product Name
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Rating value={3} readOnly />
+                      <Typography variant="body2">(3 Reviews)</Typography>
+                    </Box>
+                    <Typography
+                      sx={{ textAlign: "center", my: 2 }}
+                      variant="h5"
+                    >
+                      $40
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default HomeCatagorySection;
