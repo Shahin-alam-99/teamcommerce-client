1ef7155a3bec49b245128f2883b372fe0c1c3bbe
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-04 22:31:59 +0600

    product modal updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import CartContainer from "./Components/Cart/CartContainer/CartContainer";
 import CheckoutContainer from "./Components/Checkout/CheckoutContainer/CheckoutContainer";
 import ErrorPage from "./Components/Common/Shared/ErrorPage/ErrorPage";
 import HomeContainer from "./Components/Home/HomeContainer/HomeContainer";
-import ProductModal from "./Components/ProductModal/ProductModal";
 import Login from "./Pages/Form/Login/Login";
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/cart" element={<CartContainer />} />
           <Route path="/checkout" element={<CheckoutContainer />} />
-          <Route path="/product" element={<ProductModal />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
