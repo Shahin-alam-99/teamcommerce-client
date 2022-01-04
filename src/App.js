26c052be7a3faf32e5b142bb343006c79c92c876
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-04 22:31:26 +0600

    single productmodal added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CartContainer from "./Components/Cart/CartContainer/CartContainer";
 import CheckoutContainer from "./Components/Checkout/CheckoutContainer/CheckoutContainer";
 import ErrorPage from "./Components/Common/Shared/ErrorPage/ErrorPage";
 import HomeContainer from "./Components/Home/HomeContainer/HomeContainer";
+import ProductModal from "./Components/ProductModal/ProductModal";
 import Login from "./Pages/Form/Login/Login";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/cart" element={<CartContainer />} />
           <Route path="/checkout" element={<CheckoutContainer />} />
+          <Route path="/product" element={<ProductModal />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
